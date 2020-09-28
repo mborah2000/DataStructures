1e28f5400a85b0cfb671f8f208152a25ff5aaec1
Madhu <madhusmitaborah2000@gmail.com>
2020-09-28 11:49:09 -0700

    Latest run with maps

--- src/LifeGuards.java
+++ src/LifeGuards.java
@@ -1,9 +1,14 @@
 import java.io.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class LifeGuards {
     public static void main(String[] args){
-        try{
-            for(int file = 1 ; file <= 10 ; file++) {
+
+        for(int file = 1 ; file <= 10 ; file++) {
+            try{
                 BufferedReader reader = new BufferedReader(new FileReader(new File(file + ".in")));
                 PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(new File(file + ".out"))));
 
@@ -25,30 +30,61 @@ public class LifeGuards {
                 int totalCover = 0;
                 int[] lonewolf = new int[guards];
                 int overlap = 0;
-
+                Set<Integer> pair = null;
+                Map<Set<Integer>,Integer> map = new HashMap<>();
                 for(int i = 0; i < shifts.length ; i++){
                     lonewolf[i] = shifts[i][1] - shifts[i][0];
                     for(int j = 0 ; j < shifts.length ; j++){
                         if(i != j){
+                            pair = new HashSet<>();
                             if (shifts[j][1] < shifts[i][1] && shifts[j][0] < shifts[i][0] && shifts[j][1] > shifts[i][0]){
                                 lonewolf[i] = lonewolf[i] - (shifts[j][1] - shifts[i][0]);
-                                overlap = overlap + shifts[j][1] - shifts[i][0] ;
+                                pair.add(i);
+                                pair.add(j);
+                                if(!map.containsKey(pair)){
+                                    map.put(pair,shifts[j][1] - shifts[i][0]);
+                                } else if (map.get(pair) == 0) {
+                                    map.put(pair,shifts[j][1] - shifts[i][0]);
+                                }
                             } else if (shifts[j][1] > shifts[i][1] && shifts[j][0] < shifts[i][1] && shifts[j][0] > shifts[i][0]){
                                 lonewolf[i] = lonewolf[i] - (shifts[i][1] - shifts[j][0]);
-                                overlap = shifts[i][1] - shifts[j][0];
+                                pair.add(i);
+                                pair.add(j);
+                                if(!map.containsKey(pair)){
+                                    map.put(pair,shifts[i][1] - shifts[j][0]);
+                                } else if (map.get(pair) == 0) {
+                                    map.put(pair,shifts[i][1] - shifts[j][0]);
+                                }
                             } else if (shifts[j][1] > shifts[i][1] && shifts[j][0] < shifts[i][0]){
                                 lonewolf[i] = 0;
-                                overlap = shifts[i][1] - shifts[i][0];
+                                pair.add(i);
+                                pair.add(j);
+                                if(!map.containsKey(pair)){
+                                    map.put(pair,shifts[i][1] - shifts[i][0]);
+                                } else if (map.get(pair) == 0) {
+                                    map.remove(pair);
+                                    map.put(pair,shifts[i][1] - shifts[i][0]);
+                                }
                             } else if (shifts[i][1] > shifts[j][1] && shifts[i][0] < shifts[j][0]){
                                 lonewolf[i] = lonewolf[i] - (shifts[j][1] - shifts[j][0]);
-                                overlap = shifts[j][1] - shifts[j][0];
+                                pair.add(i);
+                                pair.add(j);
+                                if(!map.containsKey(pair)){
+                                    map.put(pair,shifts[j][1] - shifts[j][0]);
+                                } else if (map.get(pair) == 0) {
+                                    map.put(pair,shifts[j][1] - shifts[j][0]);
+                                }
                             }
                         }
                     }
-                    //System.out.println("lonewolf : " + i + " : " + lonewolf[i]);
                     totalCover = totalCover + lonewolf[i];
                 }
+                for(int value : map.values()) {
+                    overlap = overlap + value ;
+                }
                 totalCover = totalCover + overlap;
+                System.out.println("overlap : " + overlap);
+                System.out.println("totalCover : " + totalCover);
                 int result = 0;
                 for(int lone : lonewolf){
                     result = Math.max(result, totalCover - lone);
@@ -58,12 +94,13 @@ public class LifeGuards {
                 writer.println(result);
                 reader.close();
                 writer.close();
+            }catch(FileNotFoundException f){
+                f.printStackTrace();
+            }catch(IOException io){
+                io.printStackTrace();
+            }catch(OutOfMemoryError ome){
+                continue;
             }
-        }catch(FileNotFoundException f){
-            f.printStackTrace();
-        }catch(IOException io){
-            io.printStackTrace();
         }
-
     }
 }
